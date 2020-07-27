ea0b8cf4f34d6e47d4af47263e979e9ce9f7b131
MicuEmerson <micuemerson@gmail.com>
2020-07-27 23:35:21 +0300

    first version of piano

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    
+    <div class="piano-container">
+        <div class="white-note" v-for="nota in whiteNotes" :key="nota" @click="ataca(nota)">
+          <div style="margin-top: 40px;">
+            {{nota}}
+          </div>
+        </div>
     </div>
-    <router-view/>
+    
   </div>
 </template>
+
+<script>
+
+
+import { Sampler } from "tone";
+
+export default {
+
+  data: () => {
+    return {
+      sampler: Object,
+
+      whiteNotes: ["C3", "D3", "E3", "F3", "G3", "A3", "B3",
+                   "C4", "D4", "E4", "F4", "G4", "A5", "B5",
+                   "C5", "D5", "E5", "F5", "G5", "A6"],
+
+      blackNotes: ["C#3", "D#3", "F#3", "G#3", "A#3",
+                   "C#4", "D#4", "F#4", "G#4", "A#4",
+                   "C#5", "D#5", "F#5", "G#5", "A#5"]
+    };
+  },
+
+  created() {
+    this.sampler = new Sampler({
+        urls: {
+          A0:"A0.mp3", A1: "A1.mp3", A2: "A2.mp3", A3: "A3.mp3", A4: "A4.mp3", A5: "A5.mp3", A6: "A6.mp3", A7: "A7.mp3",
+          C1: "C1.mp3", C2: "C2.mp3", C3: "C3.mp3", C4: "C4.mp3", C5: "C5.mp3", C6: "C6.mp3", C7: "C7.mp3",
+        },
+        baseUrl: "https://tonejs.github.io/audio/salamander/",
+    }).toDestination();
+  },
+
+  methods: {
+    ataca(nota) {
+      this.sampler.triggerAttackRelease(nota, 2.5);
+    }
+  }
+
+}
+</script>
 
 <style>
 #app {
@@ -17,16 +61,26 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.piano-container {
+  margin-left: 30%;
+  margin-top: 20%;
+  display: flex;
+  background: gray;
+  border: 1px solid black;
+  height: 100px;
+  width: 700px;
+  display:flex;
+  /* justify-content: space-between; */
+  overflow: hidden;
+  position: relative;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.white-note {
+ background: white;
+ border: 2px solid black;
+ overflow: hidden;
+ text-align: center;
+ flex:1 0;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
